10429509b295e4c03ea460ddaafe874dfb5762d8
Sebastian Henn <138793908+SebHenn@users.noreply.github.com>
2024-09-29 14:23:36 +0200

    change to radiobuttons

---
+++ TaskTracker/Controls/ProjectItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TaskTracker.Controls
+{
+    public class ProjectItem : RadioButton
+    {
+        public static readonly DependencyProperty ProjectImageProperty =
+            DependencyProperty.Register("Image", typeof(string), typeof(ProjectItem), new PropertyMetadata(string.Empty));
+
+        public ProjectItem()
+        {
+
+        }
+
+        public string Image
+        {
+            get { return (string)GetValue(ProjectImageProperty); }
+            set { SetValue(ProjectImageProperty, value); }
+        }
+    }
+}
